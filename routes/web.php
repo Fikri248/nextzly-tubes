5c9851d7046d857c425166be3011c10e2e501a26
Fikri248 <fikriisfahani12@gmail.com>
2025-12-09 04:40:07 +0700

    autentikasi basic + redirect ke Dashboard

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,9 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/product/{id}', [ProductController::class, 'show'])->name('product.show');
 Route::get('/admin/login', [AdminAuthController::class, 'showLoginForm'])
     ->name('admin.login');
+Route::post('/admin/login', [AdminAuthController::class, 'login'])
+    ->name('admin.login.attempt');
+Route::get('/admin/dashboard', function () {
+    return 'Halo, ini halaman Dashboard admin (dummy).';
+})->name('admin.dashboard');
+
